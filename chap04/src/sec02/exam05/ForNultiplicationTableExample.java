4dc091986df1ab63f678225b34b8c5bdc3cb6800
parkjinuk09 <parkjinuk09@gmail.com>
2025-09-01 15:21:07 +0900

    04

---
+++ chap04/src/sec02/exam05/ForNultiplicationTableExample.java
@@ -0,0 +1,17 @@
+package sec02.exam05;
+
+public class ForNultiplicationTableExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int m = 2;m <= 9;m++) {
+			System.out.println("***" + m + "단 ***");
+			for(int n = 1;n <=9;n++) {
+				System.out.println(m + " x " + n + " = " + (m*n));
+
+			}
+
+		}
+	}
+
+}
