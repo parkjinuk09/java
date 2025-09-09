45f39f336dff5d1f7eeb3519abce46be48b300f2
parkjinuk09 <parkjinuk09@gmail.com>
2025-09-10 08:39:46 +0900

    수정,추가

--- chap02/src/sec01/exam02/VariableUseExample.java
+++ chap02/src/sec01/exam02/VariableUseExample.java
@@ -2,15 +2,12 @@ package sec01.exam02;
 
 public class VariableUseExample {
 
-	public static void main(String[] args) {
-		int x = 3;
-		int y = 5;
-		System.out.println("x:" + x + ", y:" + y);
-		
-		int temp = x;
-		x = y;
-		y = temp;
-		System.out.println("x:" + x + ", y:" + y);
-	}
+    public static void main(String[] args) {
+        int hour = 3;
+        int minute = 5;
+        System.out.println(hour + "시간 " + minute + "분");
 
+        int totalMinute = (hour * 60) + minute;
+        System.out.println("총 " + totalMinute + "분");
+    }
 }
