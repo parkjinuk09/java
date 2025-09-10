0d67ba121f05f84d86954a1415f239418206b0d0
parkjinuk09 <parkjinuk09@gmail.com>
2025-09-10 14:30:23 +0900

    cote

---
+++ cote/src/cote/exam18108/Main.java
@@ -0,0 +1,15 @@
+package cote.exam18108;
+
+import java.util.Scanner;
+
+public class Main {
+	 public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);  
+
+	        int a = sc.nextInt();  
+
+            System.out.println(a-543);
+
+	        sc.close();  
+	 }
+}
